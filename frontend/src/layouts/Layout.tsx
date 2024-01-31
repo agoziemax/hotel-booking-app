0383b0cf3878298633267779df72db88d0870a75
Agoziemax <agoziemax@yahoo.com>
2024-01-31 16:28:44 +0100

    updates

--- frontend/src/layouts/Layout.tsx
+++ frontend/src/layouts/Layout.tsx
@@ -11,7 +11,7 @@ function Layout({ children}: Props) {
     <div className='flex flex-col min-h-screen'>
       <Header />
       <Hero/>
-      <div className='container mx-auto py-10 border border-red-500 flex-1'>
+      <div className='container mx-auto py-10 flex-1'>
         {children}
       </div>
       <Footer/>
