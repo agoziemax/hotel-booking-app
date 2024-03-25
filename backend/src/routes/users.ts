707cbcb2c212dc93f09194e902055342bac406d9
Agoziemax <agoziemax@yahoo.com>
2024-03-25 13:51:10 +0100

    updates

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -15,9 +15,15 @@ router.post('/register', async (req: Request, res: Response) => {
     }
     user = new User(req.body);
     await user.save();
-    const token = jwt.sign({userId: user.id}, process.env.JWT_SECRET_KEY as string);
+    const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET_KEY as string, { expiresIn: '1d' });
+    res.cookie('auth_token', token, {httpOnly: true, secure: process.env.NODE_ENV === 'production',
+    maxAge: 86400000,});
+
+    return res.status(200);
   } catch (error) {
     console.log(error);
     res.status(500).send({ message: 'Something went wrong'});
   }
 });
+
+export default router;
