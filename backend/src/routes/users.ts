92e785e9729ca1056d118e919d9df8979f68f61d
Agoziemax <agoziemax@yahoo.com>
2024-04-08 16:18:51 +0100

    updates

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -1,17 +1,17 @@
-import express, { Request, Response } from 'express';
-import User from '../models/user';
-import jwt from 'jsonwebtoken';
-import { check, validationResult } from 'express-validator';
+import express, { Request, Response } from "express";
+import User from "../models/user";
+import jwt from "jsonwebtoken";
+import { check, validationResult } from "express-validator";
 
 const router = express.Router();
 
 router.post(
-  '/register',
+  "/register",
   [
-    check('firstName', 'First Name is required').isString(),
-    check('lastName', 'Last Name is required').isString(),
-    check('email', 'Email is required').isEmail(),
-    check('password', 'Password with 6 or more characters required').isLength({ min: 6 }),
+    check("firstName", "First Name is required").isString(),
+    check("lastName", "Last Name is required").isString(),
+    check("email", "Email is required").isEmail(),
+    check("password", "Password with 6 or more characters required").isLength({ min: 6 }),
   ],
   async (req: Request, res: Response) => {
     const errors = validationResult(req);
@@ -25,22 +25,23 @@ router.post(
       });
 
       if (user) {
-        return res.status(400).json({ message: 'User already exists' });
+        return res.status(400).json({ message: "User already exists" });
       }
       user = new User(req.body);
       await user.save();
-      const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET_KEY as string, { expiresIn: '1d' });
+      const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET_KEY as string, { expiresIn: "1d" });
 
-      res.cookie('auth_token', token, {
+      res.cookie("auth_token", token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
+        secure: process.env.NODE_ENV === "production",
         maxAge: 86400000,
       });
 
-      return res.sendStatus(200);
+      return res.status(200).send({ message: "User registered successfully" });
+
     } catch (error) {
       console.log(error);
-      res.status(500).send({ message: 'Something went wrong' });
+      res.status(500).send({ message: "Something went wrong" });
     }
   }
 );
