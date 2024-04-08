87910ea411f1ba3fa42656f180edc576396f71ff
Agoziemax <agoziemax@yahoo.com>
2024-04-08 18:04:35 +0100

    updates

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,14 +1,14 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
-    <div className='bg-green-600 py-6'>
-      <div className='container mx-auto flex justify-between'>
-        <span className='text-3xl text-white font-bold tracking-tight'>
-          <Link to='/'>MernHolidays.com</Link>
+    <div className="bg-green-600 py-6">
+      <div className="container mx-auto flex justify-between">
+        <span className="text-3xl text-white font-bold tracking-tight">
+          <Link to="/">MernHolidays.com</Link>
         </span>
-        <span className='flex space-x-2'>
-          <Link to='/sign-in' className='flex bg-white items-center text-green-600 px-3 font-bold hover:bg-gray-100 '>
+        <span className="flex space-x-2">
+          <Link to="/sign-in" className="flex bg-white items-center text-green-600 px-3 font-bold hover:bg-gray-100 ">
             Sign In
           </Link>
         </span>
