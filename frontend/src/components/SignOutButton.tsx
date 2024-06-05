b6b68b12a44dede1e73dc766e9842fae4d0872de
Agozie <Agoziemax@yahoo.com>
2024-06-05 14:46:06 +0100

    upddates

--- frontend/src/components/SignOutButton.tsx
+++ frontend/src/components/SignOutButton.tsx
@@ -1,24 +1,26 @@
-import { useMutation } from "react-query";
+import { useMutation, useQueryClient } from "react-query";
 import api from "../api";
 import { useNavigate } from "react-router-dom";
 import { useAppContext } from "../context/AppContext";
 import { useContext } from "react";
 
 function SignOutButton() {
+  const queryClient = useQueryClient();
   const { showToast } = useAppContext();
   const mutation = useMutation(api.signOut, {
-    onSuccess: () => {
+    onSuccess: async () => {
+      await queryClient.invalidateQueries("validateToken");
       showToast({ message: "Logged out Successfully", type: "SUCCESS" });
       navigate("/login");
-    },
+    }, 
     onError: (error: Error) => {
       showToast({ message: error.message, type: "ERROR" });
     },
   });
 
   const handleClick = () => {
-    console.log('SO test');
-    
+    console.log("SO test");
+
     mutation.mutate();
   };
   const navigate = useNavigate();
