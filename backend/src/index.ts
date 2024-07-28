0f3030415af2fd334a7cb0a52e0555f8a6f47b8b
Agoziemax <agoziemax@yahoo.com>
2024-07-28 05:36:16 +0100

    recent updates

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,6 +5,7 @@ import mongoose from "mongoose";
 import userRoutes from "./routes/users";
 import authRoutes from "./routes/auth";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 mongoose
   .connect(process.env.MONGODB_CONNECTION_STRING as string)
@@ -20,6 +21,8 @@ app.use(
     credentials: true,
   })
 );
+
+app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
 // app.get('/api/test', async (req: Request, res: Response) => {
 //   res.json({ message: 'hello from express endpoint' });
--- e2e-tests/tests/auth.spec.ts
+++ e2e-tests/tests/auth.spec.ts
@@ -2,6 +2,8 @@ import { test, expect } from "@playwright/test";
 
 const UI_URL = "http://127.0.0.1:5173/";
 
+const UI_URL_REGISTER = "http://127.0.0.1:5173/register";
+
 test("goto base URL", async ({ page }) => {
   await page.goto(UI_URL);
 
@@ -16,6 +18,28 @@ test("goto base URL", async ({ page }) => {
   await expect(page.getByRole("link", { name: "My bookings" })).toBeVisible();
   await expect(page.getByRole("link", { name: "My Hotels" })).toBeVisible();
   await expect(page.getByRole("button", { name: "Sign Out" })).toBeVisible();
+});
+
+test("register a new account", async ({ page }) => {
+  const testEmail = `test_register_${Math.floor(Math.random() * 9000) + 1000}@test.com`;
+  // Navigate to the specified URL where the registration form is expected to be
+  await page.goto(UI_URL_REGISTER);
+
+  // Check if the "Create an Account" header is visible
+  await expect(page.getByRole("heading", { name: "Create an Account" })).toBeVisible();
+
+  // Fill in the registration form fields
+  await page.locator("[name='firstName']").fill("John");
+  await page.locator("[name='lastName']").fill("Doe");
+  await page.locator("[name='email']").fill(testEmail);
+  await page.locator("[name='password']").fill("securepassword123");
+  await page.locator("[name='confirmPassword']").fill("securepassword123");
+
+  // Submit the registration form
+  await page.getByRole("button", { name: "Create account" }).click();
+
+  // Optionally, check for a success message or redirection to a login/dashboard page
+  await expect(page.getByText("Registeration Successful")).toBeVisible();
 });
 
 // test("get started link", async ({ page }) => {
