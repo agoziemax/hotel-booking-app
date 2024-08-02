b5a7356d8e1681e895fa1a797c7a188480b8445d
Agoziemax <agoziemax@yahoo.com>
2024-08-02 12:14:19 +0100

    updates

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,20 +7,13 @@ import authRoutes from "./routes/auth";
 import cookieParser from "cookie-parser";
 import path from "path";
 
-mongoose
-  .connect(process.env.MONGODB_CONNECTION_STRING as string)
-  .then(() => console.log("Connected to database:", process.env.MONGODB_CONNECTION_STRING));
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 
 const app = express();
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
